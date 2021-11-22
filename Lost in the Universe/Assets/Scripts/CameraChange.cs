49f7cd17133d55b0fb1e00a2ed62a1ea2669d6e1
Facundo Rodriguez Sabia <facundorsabia@gmail.com>
2021-11-22 19:03:16 -0300

    list y foreach

---
+++ Lost in the Universe/Assets/Scripts/CameraChange.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+    [SerializeField] private List <GameObject> cameras;
+    [SerializeField] private int indexCamera = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if(Input.GetKeyDown(KeyCode.F1))
+         {
+             indexCamera++;
+             if(indexCamera == cameras.Count)
+             {
+                 indexCamera = 0;
+             }
+             SwitchCamera(indexCamera);
+         }
+    }
+
+    void SwitchCamera (int index)
+    {
+        foreach (var camera in cameras)
+        {
+            camera.SetActive(false);
+        }
+        cameras[index].SetActive(true);
+    }
+}
